dcb91ef07d1b022b148e83c6b6bfd054d2c1ecbf
Himansh012 <devpathaksep12@gmail.com>
2025-04-12 00:02:47 +0530

    testing 1

--- HandTrackingmin.py
+++ HandTrackingmin.py
@@ -1,4 +1,5 @@
 import cv2
+from cvzone.HandTrackingModule import HandDetector
 import mediapipe as mp
 import time
 '''Use ChatGPT to understand what each line does'''
@@ -13,28 +14,31 @@ hands = mpHands.Hands()
 ### Accesses the drawing tools in mediapipe tracking the 21 points in the hand         
 mpDraw = mp.solutions.drawing_utils  
 
+detector = HandDetector(maxHands=2)
+
 pTime = 0
 cTime = 0
 
 while True:
-    success, img = cap.read()   
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
-    results = hands.process(imgRGB)
+    success, img = cap.read()
+    fimg = cv2.flip(img,1)                         
+    handd, fimg = detector.findHands(fimg)   
+    imgRGB = cv2.cvtColor(fimg, cv2.COLOR_BGR2RGB) 
+    # results = hands.process(imgRGB)
     # print(results.multi_hand_landmarks)
 
-    if results.multi_hand_landmarks:
-        for handlms in results.multi_hand_landmarks:  
-            for id, lm in enumerate(handlms.landmark):
-                # print(id,lm)
-                height, width, channel =img.shape
-                cx, cy = int(lm.x*width), int(lm.y*height)
+    # if results.multi_hand_landmarks:
+    #     for handlms in results.multi_hand_landmarks:  
+    #         for id, lm in enumerate(handlms.landmark):
+    #             # print(id,lm)
+    #             height, width, channel =img.shape
+    #             cx, cy = int(lm.x*width), int(lm.y*height)
                 # print(id,cx,cy)
                 # if id in (4,8,12):
                 #     cv2.circle(img,(cx,cy),15,(203,192,255),cv2.FILLED)
 
-            mpDraw.draw_landmarks(img,handlms,mpHands.HAND_CONNECTIONS)        
+            # mpDraw.draw_landmarks(img,handlms,mpHands.HAND_CONNECTIONS)        
 
-    fimg = cv2.flip(img,1)                         
     
     cTime = time.time()
     fps = 1/(cTime-pTime)
