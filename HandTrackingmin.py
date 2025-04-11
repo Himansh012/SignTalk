23779dc59a02daef2660328eada0504067078b18
Himansh012 <devpathaksep12@gmail.com>
2025-04-11 22:39:09 +0530

    Updated min file

--- HandTrackingmin.py
+++ HandTrackingmin.py
@@ -29,22 +29,22 @@ while True:
                 height, width, channel =img.shape
                 cx, cy = int(lm.x*width), int(lm.y*height)
                 # print(id,cx,cy)
-                if id in (4,8,12):
-                    cv2.circle(img,(cx,cy),15,(203,192,255),cv2.FILLED)
+                # if id in (4,8,12):
+                #     cv2.circle(img,(cx,cy),15,(203,192,255),cv2.FILLED)
 
             mpDraw.draw_landmarks(img,handlms,mpHands.HAND_CONNECTIONS)        
 
-    # flipimg = cv2.flip(img,1)                         
+    fimg = cv2.flip(img,1)                         
     
     cTime = time.time()
     fps = 1/(cTime-pTime)
     pTime = cTime
     
-    cv2.putText(img,"FPS->"+str(int(fps)),(20,50),cv2.FONT_HERSHEY_COMPLEX,.5,  
+    cv2.putText(fimg,"FPS->"+str(int(fps)),(20,50),cv2.FONT_HERSHEY_COMPLEX,.5,  
               (255,0,255),1)                                         
                                                                                   
 
-    cv2.imshow("Image",img)
+    cv2.imshow("Image",fimg)
     if cv2.waitKey(1) == ord('q'):
         break
 cap.release
